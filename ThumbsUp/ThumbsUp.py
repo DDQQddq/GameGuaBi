de48679eda88c0f6c6cd7cff8918867535b1bc20
Elf Dobby <63322147+DDQQddq@users.noreply.github.com>
2022-02-10 22:06:48 +0800

    ThumbsUp

---
+++ ThumbsUp/ThumbsUp.py
@@ -0,0 +1,25 @@
+import pyautogui
+import time
+
+png = r'C:\Users\elfdobby\pythonProject\GameGuaBi\ThumbsUp\03.png'
+
+pyautogui.click(405, 1055)
+pyautogui.moveTo(1100, 500)
+
+
+def kill():
+    time.sleep(0.01)
+    left, top, width, height = pyautogui.locateOnScreen(png)
+    center = pyautogui.center((left, top, width, height))
+    pyautogui.click(center)
+    pyautogui.moveTo(1100, 500)
+    print('Bingo!')
+
+
+while True:
+    if pyautogui.locateOnScreen(png):
+        kill()
+
+    else:
+        pyautogui.scroll(300)
+        print("NEXT")
